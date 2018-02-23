1721f633e3bf6588aac28d66fa845d48619298e2
AMANDA BOTELHO DE MORAES <41346828865@jpmorgan.sp>
2018-02-22 22:07:51 -0300

    EF

--- senai.ifood.domain/Entities/UsuarioPermissaoDomain.cs
+++ senai.ifood.domain/Entities/UsuarioPermissaoDomain.cs
@@ -1,10 +1,21 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace senai.ifood.domain.Entities
 {
-    public class UsuarioPermissaoDomain
+    public class UsuarioPermissaoDomain : BaseDomain
     {
+        [ForeignKey("UsuarioId")]
         public UsuarioDomain Usuario { get; set; }
+
+        [Required]
         public int UsuarioId { get; set; }
+        
+        [ForeignKey("PermissaoId")]
         public PermissaoDomain Permissao { get; set; }
+
+        [Required]
         public int PermissaoId { get; set; }
 
 
